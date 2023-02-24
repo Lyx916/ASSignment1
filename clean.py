9ecdc336d0b394e5d817ba61f2ce187930ce57cb
[liuyuxuan] <[1115601832@qq.com]>
2023-02-24 15:17:20 +0800

    data cleaning script

---
+++ clean.py
@@ -0,0 +1,21 @@
+import sys
+contact_info_file= sys.argv[1]
+other_info_file=sys.argv[2]
+output_file=sys.argv[3]
+
+import numpy
+import pandas
+import pandas as pd
+
+df_contact=pd.read_csv(contact_info_file)
+df_other=pd.read_csv(other_info_file)
+
+df=pd.merge(df_contact,df_other, left_on="respondent_id", right_on="id").drop('id',axis=1)
+
+df1=df.dropna(axis=0,how='any')
+
+mask=df1['job'].str.contains('insurance',case=False,regex=False)
+df2=df1[~mask]
+
+df2.to_csv(output_file,index=False)
+
